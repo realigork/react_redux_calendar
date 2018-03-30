dd308ab734b20c80f0130455cae8091a02756a5d
Igor Kolga <igorkolga@hotmail.com>
2018-03-30 19:06:07 +0100

    Add colour list to reminder form

--- src/components/reminder_form/index.jsx
+++ src/components/reminder_form/index.jsx
@@ -10,45 +10,75 @@ const ReminderForm = ({ day, onChange, onSubmit }) => {
 
       <form>
         <fieldset>
-          <label>
-            Start time:
-            <select id="start" onChange={onChange}>
-              <option>09:00</option>
-              <option>10:00</option>
-              <option>11:00</option>
-              <option>12:00</option>
-              <option>13:00</option>
-              <option>14:00</option>
-              <option>15:00</option>
-              <option>16:00</option>
-              <option>17:00</option>
-              <option>18:00</option>
-            </select>
+          <label className={classes.fieldRow}>
+            <span className={classes.fieldLabel}>Start time:</span>
+            <div className={classes.fieldItem}>
+              <select id="start" onChange={onChange}>
+                <option>09:00</option>
+                <option>10:00</option>
+                <option>11:00</option>
+                <option>12:00</option>
+                <option>13:00</option>
+                <option>14:00</option>
+                <option>15:00</option>
+                <option>16:00</option>
+                <option>17:00</option>
+                <option>18:00</option>
+              </select>
+            </div>
           </label>
         </fieldset>
 
         <fieldset>
-          <label>
-            End time:
-            <select id="end" onChange={onChange}>
-              <option>10:00</option>
-              <option>11:00</option>
-              <option>12:00</option>
-              <option>13:00</option>
-              <option>14:00</option>
-              <option>15:00</option>
-              <option>16:00</option>
-              <option>17:00</option>
-              <option>18:00</option>
-            </select>
+          <label className={classes.fieldRow}>
+            <span className={classes.fieldLabel}>End time:</span>
+            <div className={classes.fieldItem}>
+              <select id="end" onChange={onChange}>
+                <option>10:00</option>
+                <option>11:00</option>
+                <option>12:00</option>
+                <option>13:00</option>
+                <option>14:00</option>
+                <option>15:00</option>
+                <option>16:00</option>
+                <option>17:00</option>
+                <option>18:00</option>
+              </select>
+            </div>
           </label>
         </fieldset>
 
         <fieldset>
-          <label>Text: <input type="text" id="text" onChange={onChange} /></label>
+          <label className={classes.fieldRow}>
+            <span className={classes.fieldLabel}>Colour:</span>
+            <div className={classes.fieldItem}>
+              <ul className={classes.reminderColorList}>
+                <li className={classes.reminderYellow}></li>
+                <li className={classes.reminderGreen}></li>
+                <li className={classes.reminderBlue}></li>
+                <li className={classes.reminderRed}></li>
+              </ul>
+            </div>
+          </label>
         </fieldset>
 
-        <button className={classes.submitBtn} onClick={onSubmit}>Add</button>
+        <fieldset>
+          <label className={classes.fieldRow}>
+            <span className={classes.fieldLabel}>Text:</span>
+            <div className={classes.fieldItem}>
+              <input type="text" id="text" onChange={onChange} />
+            </div>
+          </label>
+        </fieldset>
+
+        <fieldset>
+          <label className={classes.fieldRow}>
+            <span className={classes.fieldLabel}></span>
+            <div className={classes.fieldItem}>
+              <button className={classes.submitBtn} onClick={onSubmit}>Add</button>
+            </div>
+          </label>
+        </fieldset>
       </form>
     </div>
   );
