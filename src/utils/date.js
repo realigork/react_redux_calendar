cb2714f8e5452e9309a48f1f6edf84e9eef8e47d
Igor Kolga <igorkolga@hotmail.com>
2018-03-30 14:22:50 +0100

    Add Mocha and Sinon; Update date getDays function to return trail/lead; Add date getDays test

--- src/utils/date.js
+++ src/utils/date.js
@@ -34,14 +34,29 @@ export const getMonthTotal = (year, month) => {
   return new Date(year, month + 1, 0).getDate();
 };
 
-export const getDays = (max, firstDayIndex) => {
-  console.log(firstDayIndex);
+export const getDays = (maxDays, currentDay, firstWeekdayIndex) => {
   const days = [];
-  for (let i = 1, l = max + firstDayIndex; i <= l; i++) {
-    if (i < firstDayIndex) {
-      days.push({ day: 0 });
+  const monthStart = 1;
+
+  // if it's February 28 days and first day starts on Sunday
+  // Then we only get 4 rows - 28 cells
+  // For all other cases there should be 5 rows - 35 cells
+  const maxCells = (maxDays === 28 && firstWeekdayIndex === 0) ?
+    28 - firstWeekdayIndex :
+    35 - firstWeekdayIndex;
+
+  // calculate:
+  // leading cells by deducting firstDay from current month's first day
+  // if loop index continues after current month's max days
+  // then insert trailing cells for next month
+  for (let i = monthStart - firstWeekdayIndex, l = maxCells; i <= l; i++) {
+    if (i < monthStart || i > maxDays) {
+      days.push({ day: '' });
     } else {
-      days.push({ day: i - firstDayIndex });
+      days.push({
+        day: i,
+        isCurrent: i === currentDay
+      });
     }
   }
 
