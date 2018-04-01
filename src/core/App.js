d19ca84df5ca2b3978a2c3002b7a3bfe8c652d34
Igor Kolga <igorkolga@hotmail.com>
2018-04-01 09:12:30 +0100

    Clean up src folder, move shared into Core

--- src/core/App.js
+++ src/core/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 
-import Calendar from './components/calendar';
+import Calendar from '../components/calendar';
+
+import './css/reset.css';
+import './css/base.css';
 
 import classes from './App.css';
+
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './core/App';
+import registerServiceWorker from './core/registerServiceWorker';
 import calendarReducer from './store/reducers/calendar';
-
-import './reset.css';
-import './index.css';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
